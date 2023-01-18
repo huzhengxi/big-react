9364b5a701858b1c6883f45a53204013548db140
虎正玺 <15264316179@163.com>
2023-01-18 19:31:06 +0800

    feat: 暂存

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,4 +1,18 @@
 import { jsxDEV } from './src/jsx';
+import { Dispatcher, resolveDispatcher } from './src/currentDispatcher';
+import currentDispatcher from './src/currentDispatcher';
+
+export const useState: Dispatcher['useState'] = (initialState) => {
+	const dispatcher = resolveDispatcher();
+	return dispatcher.useState(initialState);
+};
+
+
+export const __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRE = {
+	currentDispatcher
+}
+
+
 export default {
 	version: '0.0.1',
 	createElement: jsxDEV
